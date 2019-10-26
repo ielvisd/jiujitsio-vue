9249893e9019e9b7420aebb8d2ad15d5fe7173a2
Elvis Ibarra <ielvisd@gmail.com>
2019-10-25 21:01:47 -0700

    Added Vuex store

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,10 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import Vuex from "vuex";
+import App from "./App.vue";
+import router from "./router";
 
-Vue.config.productionTip = false
+Vue.use(Vuex);
+Vue.config.productionTip = false;
+
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    }
+  }
+});
+
+store.commit("increment");
+// eslint-disable-next-line no-console
+console.log(store.state.count);
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
